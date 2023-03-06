e80cfda9629d0b0c17bffd2908b681e04e98e4d4
Karasp1980 <karasp80@gmail.com>
2023-03-06 13:03:58 +0000

    Add base templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,36 @@
 from django.shortcuts import render
+from django.views import generic, View
+from .models import Post
 
-# Create your views here.
 
+class PostRecipe(generic.ListView):
+    """
+    all_recipes view
+    """
+    model = Post
+    queryset = Post.objects.order_by('-published_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
+    
+class RecipeDetails(View):
+    """ Recipe details page """
+
+    def get(self, request, slug):
+        queryset = Post.objects.all()
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments_post_name.order_by('published_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_details.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked,
+                "comment_form": CommentForm()
+            }
+        )
